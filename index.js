2f9c7020546bb10b4531e176c8f4c8f157e9ebd3
linorabolini <linorabolini@gmail.com>
2019-03-03 13:47:35 +0100

    updated with firebase

--- index.js
+++ index.js
@@ -3,30 +3,39 @@ const app = express()
 const http = require("http").Server(app)
 const io = require("socket.io")(http)
 const port = process.env.PORT || 3000
+const firebase = require("firebase")
+
+const config = {
+    apiKey: "AIzaSyBwvplEeaV5kqrvqk702CX1EI0iJCHI_JA",
+    authDomain: "livepoll-bbfa0.firebaseapp.com",
+    databaseURL: "https://livepoll-bbfa0.firebaseio.com",
+    projectId: "livepoll-bbfa0",
+    storageBucket: "",
+    messagingSenderId: "315036492171"
+}
+firebase.initializeApp(config)
+
+const database = firebase.database()
 
 app.use(express.static(__dirname + "/public"))
 
-const low = require("lowdb")
-const FileSync = require("lowdb/adapters/FileSync")
-
-const adapter = new FileSync("db.json")
-const db = low(adapter)
-
-// Set some defaults (required if your JSON file is empty)
-db.defaults({ sessions: {} }).write()
-
 const onConnection = socket => {
-    const { session } = socket.handshake.query
+    const { query, address } = socket.handshake
+    const { session } = query
+    const sessionPath = `sessions/${session}`
 
-    socket.on("vote", ({ phase, user, data }) => {
-        console.log(phase, user, data)
-        db.set(`sessions.${session}.${phase}.${user}`, data).write()
-        const newValue = db.get(`sessions.${session}`).value()
-        io.emit(`sessions.${session}`, newValue)
-    })
+    socket.on("vote", ({ phase, data }) =>
+        database.ref(`${sessionPath}/${phase}/${address}`).set(data)
+    )
 
-    const data = db.get(`sessions.${session}`, []).value()
-    socket.emit(`sessions.${session}`, data)
+    const sendData = data => {
+        // console.log("sending data", data.val())
+        socket.emit(sessionPath, data.val() || [], address)
+    }
+    const sessionRef = database.ref(sessionPath)
+
+    sessionRef.on("value", sendData)
+    socket.on("disconnect", () => sessionRef.off("value", sendData))
 }
 
 io.on("connection", onConnection)
